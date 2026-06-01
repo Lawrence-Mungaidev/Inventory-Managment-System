443e6fc37fae05d5affb5e0dc32f3f11c00f3826
Lawrence_Mungai <mungailawrence52@gmail.com>
2026-06-01 22:18:43 +0300

    Add a trycatch in sendEmail

--- src/main/java/com/Merlin/Inventory/Management/System/Email/EmailService.java
+++ src/main/java/com/Merlin/Inventory/Management/System/Email/EmailService.java
@@ -18,14 +18,19 @@ public class EmailService {
     private String fromEmail;
 
     @Async
-    public void  sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        message.setFrom(fromEmail);
-        mailSender.send(message);
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom(fromEmail);
+            mailSender.send(message);
+            System.out.println("Email sent successfully to: " + to);
+        } catch (Exception e) {
+            System.err.println("Failed to send email to: " + to);
+            e.printStackTrace();
+        }
     }
 
 }
